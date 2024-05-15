0eaddbbbf0a6e78c97f57c8fbe298b13a0929dd2
heenahan <mby1997s@gmail.com>
2024-05-15 14:42:28 +0900

    feat : Loading 페이지 구현

---
+++ src/components/LoadingView.vue
@@ -0,0 +1,37 @@
+<template>
+    <div class="loading" v-if="isLoading">
+        <img src="../assets/book_flip.gif" alt="loading"/>
+        <div class="message">책을 가져오는 중...</div>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: [ 'isLoading' ]
+    }
+</script>
+
+<style scoped>
+@import url(../assets/css/fonts.css);
+
+.loading {
+    position: relative;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 400px;
+    height: 600px;
+}
+
+.loading img {
+    width: 110px;
+    height: 110px;
+    position: absolute;
+    top: 50%;
+}
+.message {
+    font-family: 'SokchoBadaBatang', sans-serif;
+    position: absolute;
+    top: 70%;
+}
+</style>
