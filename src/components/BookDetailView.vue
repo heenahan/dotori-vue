0323dd593f6fbdbc746ea37aeb2925162a22737b
heenahan <mby1997s@gmail.com>
2024-05-16 11:55:45 +0900

    feat : 도서 상세 화면 구현

---
+++ src/components/BookDetailView.vue
@@ -0,0 +1,226 @@
+<template>
+    <nav class="top-nav">
+        <ul class="top-nav-content">
+            <li class="top-nav-button-back"><img src="../assets/back_img.png" alt="back" class="back_img" @click="handleBack"/></li>
+            <li class="top-nav-button-save"><span>저장</span></li>
+        </ul>
+    </nav>
+    <div class="container">
+        <LoadingView :isLoading="isLoading"/>
+        <div class="book">
+            <div class="book-title">{{ book.title }}</div>
+            <img :src="book.coverPath" alt="book"/>
+            <div class="book-author">{{ book.author }}</div>
+            <div class="book-publisher">{{ book.publisher }}</div>
+        </div>
+        <ul class="book-info">
+            <li class="book-info-item">
+                <div class="book-info-tag">카테고리</div>
+                <div class="book-info-context">{{ book.categoryName }}</div>
+            </li>
+            <li class="book-info-item">
+                <div class="book-info-tag">페이지 수</div>
+                <div class="book-info-context">{{ book.page }}</div>
+            </li>
+            <li class="book-info-item">
+                <div class="book-info-tag">ISBN</div>
+                <div class="book-info-context">{{ book.isbn13 }}</div>
+            </li>
+            <li class="book-info-item">
+                <div class="book-info-tag">출판일</div>
+                <div class="book-info-context">{{ book.publishDate }}</div>
+            </li>
+        </ul>
+        <div>
+            <div class="book-description">
+                <div class="book-description-tag">책 소개</div>
+                <div class="book-description-context">{{ book.description }}</div>
+            </div>
+            <div class="book-link"><a :href="book.link">책 구매 링크</a></div>
+            <div class="database-tag">도서 DB 제공: 알라딘</div>
+        </div>
+    </div>
+    <NavigationView/>
+</template>
+
+<script>
+import LoadingView from './LoadingView.vue';
+import { useRoute, useRouter } from 'vue-router'
+import axios from 'axios'
+import NavigationView from './NavigationView.vue'
+
+export default {
+    components: {
+        LoadingView,
+        NavigationView
+    },
+    beforeMount() {
+        const route = useRoute();
+        const { isbn } = route.params;
+        axios.get(process.env.VUE_APP_DOTORI_API_URL + '/books/' + isbn, {
+            headers: {
+                Authorization: localStorage.getItem('accessToken')
+            }
+        }).then((response) => {
+            this.isLoading = false;
+            this.book = response.data.data;
+            console.log(this.book);
+        }).catch((error) => {
+            console.error(error);
+        });
+    },
+    data() {
+        return {
+            router: useRouter(),
+            isLoading: true,
+            book: {}
+        }
+    },
+    methods: {
+        handleBack() {
+            this.router.back();
+        }
+    }
+}
+</script>
+
+<style scoped>
+@import url(../assets/css/fonts.css);
+
+.top-nav {
+    position: fixed;
+    top: 0; left: 0;
+    width: 412px;
+    padding: 0;
+    background-color: white;
+}
+
+.top-nav-content {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+}
+
+.top-nav-content li {
+    display: inline-block;
+}
+
+.top-nav-button-back {
+    padding: 8px;
+    width: 20px; 
+    height: 20px;
+}
+
+.top-nav-button-save {
+    float: right;
+    font-size: 18px;
+    font-weight: bold;
+    padding: 12px 8px 8px 8px;
+    color: #F28379;
+    font-family: 'SokchoBadaBatang', sans-serif;
+}
+
+.container {
+    font-family: 'SokchoBadaBatang', sans-serif;
+    width: 396px;
+    padding-top: 100px;
+    padding-bottom: 60px;
+}
+
+.container::-webkit-scrollbar {
+    display: none;
+}
+
+.book {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+}
+
+.book img {
+    width: 200px;
+    height: 250px;
+    border: 0;
+    border-radius: 10px;
+    box-shadow: 0px 3px 4px rgba(0, 0, 0, 0.3);
+}
+
+.book-title {
+    margin-bottom: 30px;
+    font-size: 20px;
+    font-style: bold;
+}
+
+.book-author, .book-publisher {
+    margin-top: 20px;
+    font-size: 16px;
+    color: #666;
+}
+
+.book-publisher {
+    margin-bottom: 40px;
+}
+
+.book-info {
+    display: flex;
+    overflow-x: auto; 
+    padding: 0;
+    list-style: none; 
+}
+
+.book-info::-webkit-scrollbar{
+  display:none;
+}
+
+.book-info-item {
+    display: inline-block;
+}
+
+.book-info-tag {
+    font-weight: bold;
+    margin-bottom: 8px;
+    font-size: 17px;
+    color: #00B992;
+}
+
+.book-info-context {
+    color: #666;
+    font-size: 16px;
+
+}
+
+.book-info-tag, .book-info-context {
+    width: 155px;
+    text-align: center;
+}
+
+.book-description {
+    margin-top: 50px;
+    margin-bottom: 30px;
+    text-align: left;
+}
+
+.book-description-tag {
+    font-weight: bold;
+    margin-bottom: 15px;
+    font-size: 20px;
+    color: #00B992;
+}
+
+.book-description-context {
+    font-size: 16px;
+    color: #666;
+}
+
+.book-link>a:link, .book-link>a:visited {
+    font-size: 16px;
+    color: #00B992;
+}
+
+.database-tag {
+    text-align: right;
+    color: #666;
+}
+</style>
--- src/components/SearchBookView.vue
+++ src/components/SearchBookView.vue
@@ -2,12 +2,14 @@
     <div class="search-view" @scroll="handleScroll">
         <input type="text" placeholder="책 검색하기" class="search-bar" v-model="query" @keyup.enter="submit">
         <LoadingView :isLoading="isLoading"/>
-        <div v-for="(book, bookId) in books" :key="bookId" class="book-item">
-            <img :src="book.coverPath" alt="#" class="book-image">
-            <div class="book-info">
-                <div class="book-title">{{ book.title }}</div>
-                <div class="book-author">{{ book.author }}</div>
-                <div class="book-publisher">{{ book.publisher }}</div>
+        <div class="book-items">
+            <div v-for="book in books" :key="book.isbn13" class="book-item" @click="showBookDetail(book.isbn13)">
+                <img :src="book.coverPath" alt="#" class="book-image">
+                <div class="book-info">
+                    <div class="book-title">{{ book.title }}</div>
+                    <div class="book-author">{{ book.author }}</div>
+                    <div class="book-publisher">{{ book.publisher }}</div>
+                </div>
             </div>
         </div>
     </div>
@@ -17,6 +19,7 @@
 <script>
 import NavigationView from './NavigationView.vue'
 import LoadingView from './LoadingView.vue';
+import { useRouter } from 'vue-router'
 import axios from 'axios'
 
 export default {
@@ -26,6 +29,7 @@ export default {
     },
     data() {
         return {
+            router: useRouter(),
             books: [],
             isLoading: false,
             page: 1,
@@ -76,6 +80,9 @@ export default {
             
             console.log(this.query);
             this.addBookList();
+        },
+        showBookDetail(isbn) {
+            this.router.push('/book/' + isbn);
         }
     }
 }
@@ -86,14 +93,19 @@ export default {
 
 .search-view {
     font-family: 'SokchoBadaBatang', sans-serif;
-    height: calc(100vh - 65px);
+    height: calc(100vh - 65px); 
     overflow: auto;
+    width: 396px;
+}
+
+.search-view::-webkit-scrollbar {
+    display: none;
 }
 
 .search-bar {
-    width: 380px;
+    width: 97.5%;
     height: 40px;
-    padding-left: 12px;
+    padding-left: 5px;
     margin-bottom: 5px;
     border: 1px solid #ccc;
     border-radius: 5px;
@@ -104,39 +116,41 @@ export default {
     color: #999;
 }
 
+.book-items {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    width: 100%;
+}
+
 .book-item {
     display: flex;
-    flex-wrap: wrap;
-    justify-content: space-around;
-    align-items: center;
     margin-bottom: 5px;
-    height: 130px;
-    width: 395px;
-    
-    border: 0px solid white;
+    width: 98.5%;
+
+    border: 0px;
     border-radius: 10px;
-    box-shadow: 0px 2px 4px rgba(0, 0, 0, 0.2);
+    box-shadow: 2px 2px 4px rgba(0, 0, 0, 0.2);
 }
 
 .book-image {
     width: 100px;
     height: 130px;
     margin-right: 10px;
-    border: 0.1px solid white;
+    border: 0px;
     border-top-left-radius: 10px;
     border-bottom-left-radius: 10px;
 }
 
 .book-info {
     height: 130px;
-    margin-right: 5px;
     display: flex;
     flex-direction: column;
     justify-content: space-around;
     overflow: hidden;
     flex: 1;
 }
-.book-info > div {
+.book-title, .book-author, .book-publisher{
     overflow: hidden; 
     text-overflow: ellipsis;
     display: -webkit-box;
