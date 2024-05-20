af8a99abeff805c93aae93a8d58f1a9174f9c340
heenahan <mby1997s@gmail.com>
2024-05-21 00:14:02 +0900

    feat : 독서 기록 상세 중 도서 정보 화면 구현

--- src/components/member_book/MemberBookDetailView.vue
+++ src/components/member_book/MemberBookDetailView.vue
@@ -1,20 +1,28 @@
 <template>
-    <div>
-        <img :src="memberBook.coverPath" />
-        <div>{{ memberBook.title }}</div>
-        <div>{{ memberBook.author }}</div>
-        <div>{{ memberBook.publisher }}</div>
-        <div>{{ memberBook.categoryName }}</div>
+    <TopNavigationView :message="message"/>
+    <div class="member-book-detail">
+        <div class="book-title">{{ memberBook.title }}</div>
+        <div class="book-info">
+            <div class="book-image"><img :src="memberBook.coverPath" /></div>
+            <div class="book-info-detail">
+                <div class="book-author"><span style="font-weight: bold;">지은이</span> | {{ memberBook.author }}</div>
+                <div class="book-publisher"><span style="font-weight: bold;">출판사</span> | {{ memberBook.publisher }}</div>
+                <div class="book-category"><span>#{{ memberBook.categoryName }}</span></div>
+            </div>
+        </div>
         <MemberBookToReadView :memberBook="memberBook" v-if="memberBook.memberBookStatus === 'TO_READ'"/>
         <MemberBookReadingView :memberBook="memberBook" v-else-if="memberBook.memberBookStatus === 'READING'"/>
         <MemberBookReadView :memberBook="memberBook" v-else/>
     </div>
+    <BottomNavigationView />
 </template>
 
 <script>
 import MemberBookToReadView from './MemberBookToReadView.vue';
 import MemberBookReadingView from './MemberBookReadingView.vue';
 import MemberBookReadView from './MemberBookReadView.vue';
+import TopNavigationView from '../TopNavigationView.vue';
+import BottomNavigationView from '../BottomNavigationView.vue';
 import axios from 'axios';
 import { useRoute } from 'vue-router'
 
@@ -22,7 +30,9 @@ export default {
     components: {
         MemberBookToReadView,
         MemberBookReadingView,
-        MemberBookReadView
+        MemberBookReadView,
+        TopNavigationView,
+        BottomNavigationView
     },
     beforeMount() {
         console.log(this.route.params);
@@ -41,12 +51,61 @@ export default {
     data() {
         return {
             route: useRoute(),
-            memberBook: {}
+            memberBook: {},
+            message: '수정하기'
         }
     }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.member-book-detail {
+    margin-top: 100px;
+    width: 396px;
+}
 
+.book-title {
+    font-size: 20%;
+    font-weight: bold;
+    width: 290px;
+    text-align: center;
+    margin: 0 auto;
+    color: #444;    
+}
+
+.book-info {
+    display: flex;
+    align-items: flex-end;
+    margin-top: 30px;
+    margin-bottom: 20px;
+}
+
+.book-image > img{
+    width: 160px;
+    height: 210px;
+    border-radius: 10px;
+    box-shadow: 2px 3px 3px 0 rgba(0, 0, 0, 0.2);
+}
+
+.book-info-detail {
+    margin-left: 20px;
+    bottom: 0;
+    color: #444;
+}
+
+.book-author, .book-publisher{ 
+    font-size: 16px;
+    color: #666;
+    margin-bottom: 15px;
+}
+
+.book-category span {
+    padding: 3px;
+    text-align: center;
+    font-size: 16px;
+    color: white;
+    border-radius: 5px;
+    background-color: #9CD9C8;
+    
+}
 </style>
