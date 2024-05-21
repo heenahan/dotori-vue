93c6e974ab939e248e0cc9c46fd55d298e53ac89
heenahan <mby1997s@gmail.com>
2024-05-21 18:34:10 +0900

    feat : 독서 기록 상태에 따른 상세 화면 구현

--- src/components/member_book/MemberBookReadingView.vue
+++ src/components/member_book/MemberBookReadingView.vue
@@ -1,16 +1,164 @@
 <template>
-    <div>
-        <div>읽는 중</div>
+    <div class="book-reading-info">
+      <div class="read-status"><span class="read-status-content">🌷읽는 중</span></div>
+      <div class="read-rate">
+        <div class="read-rate-tag">독서량</div>
+        <div class="read-rate-message">{{ convertReadRateMessage }}</div>
+        <progress class="progress-bar" :value="memberBook.percent" min="0" max="100"/>
+      </div>
+      <div class="read-date">
+        <div class="read-date-tag">독서 기간</div>
+        <div class="read-duration">
+          <div class="calculated-read-date">{{ calculateReadDate }}</div>
+          <div class="calculated-read-date-content">일 동안 읽었어요!</div>
+        </div>
+      </div>
+      <div class="read-date-detail">
+        <div class="start-date">
+          <div class="start-date-tag">시작일</div>
+          <div class="start-date-content">{{ memberBook.startDate }}</div>
+        </div>
+        <div class="end-date">
+          <div class="end-date-tag">종료일</div>
+          <div class="end-date-content">-</div>
+        </div>
+      </div>
     </div>
 </template>
 
 <script>
 export default {
     props: [ 'memberBook' ],
-    
+    computed: {
+        calculateReadDate() {
+            const startDate = new Date(this.memberBook.startDate);
+            const endDate = new Date();
+            const day = (endDate - startDate) / (1000 * 60 * 60 * 24);
+            return Math.ceil(day);
+        },
+        convertReadRateMessage() {
+          const percent = this.memberBook.percent;
+          if (percent === 0) {
+            return "🧘 독서를 시작해봐요.";
+          }
+          if (percent < 35) {
+            return "🚶 책 맛보기 중"
+          }
+          if (percent < 70) {
+            return "🔥 탐독하는 중"
+          }
+          if (percent < 100) {
+            return "🏃 완독을 향해 달려가는 중"
+          }
+          return "🎉 책을 다 읽었어요!";
+        }
+    }
 }
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.book-reading-info {
+  font-size: 15px;
+  color: #555;
+  margin-top: 50px;
+}
 
+.read-status {
+  text-align: center;
+}
+
+.read-status-content {
+  background-color: #9CD9C8;
+  border: 0px;
+  border-radius: 5px;
+  color: white;
+  padding: 3px 2px 1px 1px;
+  text-align: center;
+}
+
+.read-rate {
+  margin-top: 40px;
+}
+
+.read-rate-tag {
+  font-weight: bold;
+}
+
+.read-rate-message {
+  margin-top: 15px;
+  margin-bottom: 5px;
+}
+
+.progress-bar {
+  width: 100%;
+  appearance: none;
+}
+
+.progress-bar::-webkit-progress-bar {
+  background-color: #F2EDE4;
+  border-radius: 5px;
+}
+
+.progress-bar::-webkit-progress-value {
+  background-color: #9CD9C8;
+  border-radius: 5px;
+}
+
+.read-date {
+  color: #555;
+  margin-top: 50px;
+  display: flex;
+  align-items: center;
+}
+
+.read-date-tag {
+  padding-top: 1px;
+  font-weight: bold;
+}
+
+.read-duration {
+  margin-left: auto;
+  display: flex;
+}
+
+.calculated-read-date {
+  background-color: #F28379;
+  width: 50px;
+  border: 0px;
+  padding-top: 1px;
+  margin-right: 2px;
+  border-radius: 5px;
+  text-align: center;
+  font-weight: bold;
+  color: white;
+}
+
+.read-date-detail {
+  color: #555;
+  height: 50px;
+  margin-top: 20px;
+  display: flex;
+  align-items: center; 
+  border: 0px;
+  border-radius: 5px;
+  background-color: #F2EDE4;
+}
+
+.start-date, .end-date {
+  display: flex;
+}
+
+.start-date {
+  margin-left: 10px;
+}
+
+.end-date {
+  margin-left: auto;
+  margin-right: 10px;
+}
+
+.start-date-tag, .end-date-tag {
+  margin-right: 8px;
+  font-weight: bold;
+}
 </style>
--- src/components/member_book/MemberBookToReadView.vue
+++ src/components/member_book/MemberBookToReadView.vue
@@ -1,7 +1,13 @@
 <template>
-    <div>
-        <div>읽을 예정</div>
-        <div>{{ `이 책을 읽으면 ${calculateHeight}cm만큼 쌓여요.` }}</div>
+    <div class="book-to-read-info">
+        <div class="read-status"><span class="read-status-content">🌱읽을 예정</span></div>
+        <div class="book-page">
+        <img src="@/assets/dotori_logo.png" alt="dotori_logo" class="dotori-logo">
+        <div class="book-page-content">
+          <div class="book-page-tag">도토리에서 더 많은 책을 읽어보세요📚</div>  
+          <div class="book-page-hegiht">책을 읽어서 <span>{{ calculateHeight }}</span>cm만큼 쌓였어요.</div>      
+        </div>
+      </div>
     </div>
 </template>
 
@@ -19,5 +25,48 @@ export default {
 </script>
 
 <style scoped>
+.book-to-read-info {
+  font-size: 15px;
+  color: #555;
+  margin-top: 50px;
+}
 
+.read-status {
+  text-align: center;
+}
+
+.read-status-content {
+  background-color: #9CD9C8;
+  border: 0px;
+  border-radius: 5px;
+  color: white;
+  padding: 3px 2px 1px 1px;
+  text-align: center;
+}
+
+.book-page {
+  margin-top: 20px;
+  background-color: #F2EDE4;
+  padding: 8px 0px 8px 5px;
+  border-radius: 5px;
+  display: flex;
+}
+
+.dotori-logo {
+  width: 40px;
+  height: 40px;
+}
+
+.book-page-content {
+  margin-left: 5px;
+}
+
+.book-page-tag {
+  font-weight: bold;
+  margin-bottom: 5px;
+}
+
+.book-page-hegiht span {
+  color: #F28379;
+}
 </style>
