29fc187ea6f3af5a7206074d17c0c87069e151ad
heenahan <mby1997s@gmail.com>
2024-05-22 19:55:59 +0900

    build : v-calendar 추가

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router/index.js'
+import { setupCalendar, Calendar, DatePicker } from 'v-calendar'
+import 'v-calendar/style.css'
 
 const app = createApp(App)
 
 app.use(router)
+app.use(setupCalendar, {})
+
+app.component('VCalendar', Calendar)
+app.component('VDatePicker', DatePicker)
 
 app.mount('#app')
